f563b5b85831e17962262b3f88b61592da6d96c4
Marcelo Amaral <62728232+MarceloSAmaral@users.noreply.github.com>
2022-07-23 16:55:34 -0300

    Creating AppServices library

---
+++ ToDoSample/ToDoApp.AppServices/ToDoItemsApplication.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToDoApp.Base;
+using ToDoApp.CoreObjects.AppInterfaces;
+using ToDoApp.CoreObjects.Entities;
+using ToDoApp.CoreObjects.RepoInterfaces;
+
+
+namespace ToDoApp.AppServices
+{
+    public class ToDoItemsApplication : IToDoItemsApplication
+    {
+        protected IServiceProvider ServiceProvider { get; }
+        protected IUnitOfWorkFactory UowFactory { get; private set; }
+
+        public ToDoItemsApplication(IServiceProvider serviceProvider, IUnitOfWorkFactory workFactory)
+        {
+            ServiceProvider = serviceProvider;
+            UowFactory = workFactory;
+        }
+
+        public async Task CreateToDoItemAsync(User currentUser, ToDoItem toDoItem)
+        {
+            if (currentUser.Id != toDoItem.UserId) throw new NotAuthorizedException();
+
+            using (var uow = UowFactory.Create())
+            {
+                var alreadyExistingItem = await uow.ToDoItemsRepository.GetByKeyAsync(toDoItem.Id);
+                if (alreadyExistingItem != null) return;
+
+                IUsersApplication usersApplication = ServiceProvider.GetService<IUsersApplication>();
+                var user = await usersApplication.GetUserByIdAsync(toDoItem.UserId);
+                if (user == null) throw new UserNotFoundException();
+
+                if (toDoItem.Completed) throw new ToDoItemCannotBeCreatedAlreadyCompletedException();
+
+                toDoItem.CreatedAt = TimeProvider.Current.OffsetUtcNow;
+                toDoItem.UpdatedAt = toDoItem.CreatedAt;
+
+                uow.ToDoItemsRepository.Insert(toDoItem);
+
+                await uow.CommitAsync();
+            }
+        }
+
+        public async Task UpdateToDoItemAsync(User currentUser, ToDoItem toDoItem)
+        {
+            if (currentUser.Id != toDoItem.UserId) throw new NotAuthorizedException();
+
+            using (var uow = UowFactory.Create())
+            {
+                var alreadyExistingItem = await uow.ToDoItemsRepository.GetByKeyAsync(toDoItem.Id);
+                if (alreadyExistingItem == null) throw new ToDoItemNotFoundException();
+
+                IUsersApplication usersApplication = ServiceProvider.GetService<IUsersApplication>();
+                var user = await usersApplication.GetUserByIdAsync(toDoItem.UserId);
+                if (user == null) throw new UserNotFoundException();
+
+                if (alreadyExistingItem.Completed) throw new CannotUpdateCompletedToDoItemException();
+
+                alreadyExistingItem.UpdatedAt = TimeProvider.Current.OffsetUtcNow;
+                alreadyExistingItem.ItemContent = toDoItem.ItemContent;
+
+                uow.ToDoItemsRepository.Update(alreadyExistingItem);
+
+                await uow.CommitAsync();
+            }
+        }
+
+        public async Task CompleteToDoItemAsync(User currentUser, Guid toDoItemId)
+        {
+            using (var uow = UowFactory.Create())
+            {
+                var alreadyExistingItem = await uow.ToDoItemsRepository.GetByKeyAsync(toDoItemId);
+                if (alreadyExistingItem == null) throw new ToDoItemNotFoundException();
+
+                if (currentUser.Id != alreadyExistingItem.UserId) throw new NotAuthorizedException();
+
+                if (alreadyExistingItem.Completed) return;
+
+                alreadyExistingItem.CompletedAt = TimeProvider.Current.OffsetUtcNow;
+                alreadyExistingItem.Completed = true;
+
+                uow.ToDoItemsRepository.Update(alreadyExistingItem);
+
+                await uow.CommitAsync();
+            }
+        }
+
+        public async Task DeleteToDoItemAsync(User currentUser, Guid toDoItemId)
+        {
+            using (var uow = UowFactory.Create())
+            {
+                var alreadyExistingItem = await uow.ToDoItemsRepository.GetByKeyAsync(toDoItemId);
+                if (alreadyExistingItem == null) return;
+
+                if (currentUser.Id != alreadyExistingItem.UserId) throw new NotAuthorizedException();
+
+                uow.ToDoItemsRepository.Delete(toDoItemId);
+
+                await uow.CommitAsync();
+            }
+        }
+
+        public async Task<ToDoItem> GetItemByIdAsync(Guid toDoItemId)
+        {
+            using (var uow = UowFactory.Create())
+            {
+                return await uow.ToDoItemsRepository.GetByKeyAsync(toDoItemId);
+            }
+        }
+
+        public IEnumerable<ToDoItem> GetItems(User currentUser)
+        {
+            using (var uow = UowFactory.Create())
+            {
+                return uow.ToDoItemsRepository.Get(x => x.UserId == currentUser.Id).AsEnumerable();
+            }
+        }
+    }
+
+
+}
---
+++ ToDoSample/ToDoApp.AppServices/UserAppService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using ToDoApp.Base;
+using ToDoApp.CoreObjects.AppInterfaces;
+using ToDoApp.CoreObjects.Entities;
+using ToDoApp.CoreObjects.RepoInterfaces;
+
+
+namespace ToDoApp.AppServices
+{
+    public class UserAppService : IUsersApplication
+    {
+        protected IServiceProvider ServiceProvider { get; }
+        protected IUnitOfWorkFactory UowFactory { get; private set; }
+
+        public UserAppService(IServiceProvider serviceProvider, IUnitOfWorkFactory workFactory)
+        {
+            ServiceProvider = serviceProvider;
+            UowFactory = workFactory;
+        }
+
+        public async Task<User> GetUserByIdAsync(Guid id)
+        {
+            using (var uow = UowFactory.Create())
+            {
+                var userData = await uow.UsersRepository.GetByKeyAsync(id);
+                if (userData == null) throw new UserNotFoundException();
+                return userData;
+            }
+        }
+    }
+
+
+}
